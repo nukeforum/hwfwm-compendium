cdcf74e7966368abb545a91a8ec3bd8af54acd4a
Ben Zweber <codemunkie@gmail.com>
2022-08-11 21:14:09 -0500

    add clear button for search box

--- app/src/main/java/com/mobile/wizardry/compendium/essenceinfo/EssenceSearch.kt
+++ app/src/main/java/com/mobile/wizardry/compendium/essenceinfo/EssenceSearch.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mobile.wizardry.compendium.EssenceProvider
 import com.mobile.wizardry.compendium.LocalNavController
 import com.mobile.wizardry.compendium.Nav
+import com.mobile.wizardry.compendium.R
 import com.mobile.wizardry.compendium.essences.model.Essence
 import com.mobile.wizardry.compendium.essences.model.Rarity
 
@@ -58,6 +62,13 @@ fun EssenceSearch(essenceProvider: EssenceProvider) {
             onValueChange = { filter = it },
             modifier = Modifier
                 .fillMaxWidth(),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_x),
+                    contentDescription = stringResource(R.string.clear_search_accessibility),
+                    modifier = Modifier.clickable { filter = "" }
+                )
+            }
         )
     }
 }
