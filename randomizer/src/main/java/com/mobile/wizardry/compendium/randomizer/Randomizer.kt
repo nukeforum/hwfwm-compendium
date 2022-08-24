d9ee87579edff750d45c6f30f45ec83b712321f9
Ben Zweber <codemunkie@gmail.com>
2022-08-23 21:27:16 -0500

    clean up randomizer feature

--- randomizer/src/main/java/com/mobile/wizardry/compendium/randomizer/Randomizer.kt
+++ randomizer/src/main/java/com/mobile/wizardry/compendium/randomizer/Randomizer.kt
@@ -1,8 +1,7 @@
 package com.mobile.wizardry.compendium.randomizer
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,7 +9,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.mobile.wizardry.compendium.essences.model.Essence
 import com.mobile.wizardry.compendium.model.core.UiResult
 
 @Composable
@@ -19,18 +20,58 @@ fun Randomizer(
 ) {
     val state by viewModel.state.collectAsState()
 
-    if (state !is UiResult.Success) return
+    Column(modifier = Modifier.fillMaxSize()) {
+        when (state) {
+            is UiResult.Success -> {
+                val uiState = remember { state.data }
 
-    val uiState = remember { state.data }
+                RandomizerResult(
+                    uiState.randomizedSet,
+                    uiState.knownConfluence,
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                )
+            }
+            is UiResult.Error -> TODO()
+            UiResult.Loading -> Loading(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+        }
 
+        Button(
+            modifier = Modifier.fillMaxWidth().height(100.dp),
+            onClick = { viewModel.randomize() }
+        ) {
+            Text(text = "Randomize Essences")
+        }
+    }
+}
+
+@Composable
+private fun RandomizerResult(
+    randomizedSet: Set<Essence.Manifestation>,
+    confluence: Essence.Confluence?,
+    modifier: Modifier = Modifier,
+) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        uiState.randomizedSet.forEach {
-            Text(text = it.name)
-        }
-        Text(text = uiState.knownConfluence?.name ?: "No known Confluence")
+        randomizedSet.forEach { Text(text = it.name) }
+        Text(text = confluence?.name ?: "No known Confluence")
+    }
+}
+
+@Composable
+private fun Loading(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = "Loading")
     }
 }
