54a94dc8b563591ae73a1b7b830e52fb3b95f204
Ben Zweber <codemunkie@gmail.com>
2026-05-04 16:11:15 -0500

    allow creation of racial abilities

--- ability-listing-contributions/src/main/java/wizardry/compendium/abilitylisting/contributions/AbilityListingContributionsScreen.kt
+++ ability-listing-contributions/src/main/java/wizardry/compendium/abilitylisting/contributions/AbilityListingContributionsScreen.kt
@@ -871,7 +871,7 @@ private inline fun <reified T : Any> sealedObjects(): List<T> =
     T::class.sealedSubclasses.mapNotNull { it.objectInstance }
 
 private val AbilityTypeOptions: List<AbilityType> =
-    sealedObjects<AbilityType>().filterNot { it === AbilityType.Use || it === AbilityType.RacialAbility }
+    sealedObjects<AbilityType>().filterNot { it === AbilityType.Use }
 
 private val AmountOptions: List<Amount> = sealedObjects<Amount>()
 
