01d422231b2a836ade3ef983bf9a85e54efbbbf3
Ben Zweber <codemunkie@gmail.com>
2026-05-05 16:16:02 -0500

    fix(design): use onSurfaceVariant for essence highlight (dark mode)

--- design/src/main/java/wizardry/compendium/ui/theme/Color.kt
+++ design/src/main/java/wizardry/compendium/ui/theme/Color.kt
@@ -1,16 +1,12 @@
 package wizardry.compendium.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 
 val RestrictedRed = Color.Red.copy(alpha = 0.5f)
 
 @Composable
 fun essenceHighlight(isRestricted: Boolean): Color {
-    return if (isRestricted) RestrictedRed else Color.DarkGray
+    return if (isRestricted) RestrictedRed else MaterialTheme.colorScheme.onSurfaceVariant
 }
